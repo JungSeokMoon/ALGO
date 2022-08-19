8d75a49bbe277dbbddd4fd4ea4ef433c5ed00bfd
JungSeokMoon <46036612+JungSeokMoon@users.noreply.github.com>
2022-08-19 13:45:12 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+/*
+문제
+세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+
+예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+
+세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+출력
+첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+
+3
+40 80 60
+최대값 : 80
+
+40/80*100 + 80/80*100 + 60/80*100
+= 50 + 100 + 75
+= 225 / 3 = 75
+
+75.0
+
+3
+10 20 30
+
+10/30 * 100 + 20/30 * 100 + 100
+
+33.333 + 66.666
+199.999/3 66.66667
+
+66.666667
+ */
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw  = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		double[] arr = new double[N];
+		
+		String[] str = br.readLine().split(" ");
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		
+		Arrays.sort(arr);
+		double sum = 0.0;
+		for(int i=0; i<N; i++) {
+			sum += ((arr[i]/arr[N-1])*100);
+		}
+		
+		bw.write((sum/N)+"");
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
