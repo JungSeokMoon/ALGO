ca704221111c22e933d567fb5a995b9353dca646
JungSeokMoon <46036612+JungSeokMoon@users.noreply.github.com>
2022-08-25 15:53:49 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 144 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+/*
+문제
+알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
+
+출력
+각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
+
+만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
+
+baekjoon
+
+1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		int size = str.length();
+		int[] arr = new int[26];
+		Arrays.fill(arr, -1);
+		int idx = 0;
+		for(int i=0; i<size; i++) {
+			idx = str.charAt(i) - 'a';
+			if(arr[idx] != -1) continue;
+			arr[idx] = i;
+		}
+		
+		for(int i=0; i<26; i++) {
+			bw.write(arr[i]+" ");
+		}
+		
+		bw.flush();
+		bw.close();
+		
+		
+		
+		
+
+	}
+
+}
